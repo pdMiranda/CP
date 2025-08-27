a22ec231177459f2ab9a034eb98e85f26b96c722
pdMiranda <pedromrodrigues1807@gmail.com>
2025-08-27 14:54:00 -0300

    update

--- Tarefa_02/main.c
+++ Tarefa_02/main.c
@@ -3,11 +3,24 @@
 
 int main()
 {
-    #pragma omp parallel for num_threads(2) schedule(static) // paraleliza o loop com 2 threads
-    for(int i = 1; i <= 3; i++) 
+    int i;
+
+    #pragma omp parallel num_threads(2) // seta o número de threads em 2 
     {
-        int tid = omp_get_thread_num(); // lê o identificador da thread
-        printf("[PRINT1] T%d = %d \n", tid, i);
-        printf("[PRINT2] T%d = %d \n", tid, i);
+        int tid = omp_get_thread_num(); // lê o identificador da thread 
+
+        // distribui loop entre as threads (MAP) de forma dinamica
+        #pragma omp for ordered schedule(dynamic) 
+
+        for(i = 1; i <= 3; i++) 
+        {
+            // forca a ordem para os eventos do loop (ordered clause)
+           #pragma omp ordered 
+
+           {
+               printf("[PRINT1] T%d = %d \n",tid,i);
+               printf("[PRINT2] T%d = %d \n",tid,i);
+           }
+        }
     }
 }
