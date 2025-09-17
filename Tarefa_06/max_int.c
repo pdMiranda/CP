f91a42753b1f9234f2cc638084b3c3ab5b8f383f
pdMiranda <pedromrodrigues1807@gmail.com>
2025-09-17 15:01:32 -0300

    update

--- Tarefa_06/max_int.c
+++ Tarefa_06/max_int.c
@@ -1,5 +1,6 @@
 #include <time.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <mpi.h>
 
 #define N 10
@@ -17,17 +18,39 @@ void main(int argc, char* argv[]) {
 
   if (rank == 0) {
     // preencher o buffer com N valores inteiros aleatórios
+    srand(time(NULL));
+    for (int i = 0; i < N; i++) {
+      buffer[i] = rand() % 100; // Valores aleatórios de 0 a 99
+    }
   } 
 
   // distribuir o vetor para todos os outros processos
+  MPI_Bcast(buffer, N, MPI_INT, 0, MPI_COMM_WORLD);
 
   // processar o maior dos valores dentro do seu intervalo
+  maior_parcial = -1; // Inicializa com um valor menor que qualquer possível no buffer
+  for (int i = rank; i < N; i += numProcs) {
+    if (buffer[i] > maior_parcial) {
+      maior_parcial = buffer[i];
+    }
+  }
 
   // reduzir os maiores no maior, enviando o resultado para o processo com rank = 0
+  MPI_Reduce(&maior_parcial, &maior_final, 1, MPI_INT, MPI_MAX, 0, MPI_COMM_WORLD);
 
   if (rank == 0) {
     // imprimir maior
+    // imprimindo o vetor inteiro para verificacao.
+    printf("Vetor: [ ");
+    for(int i = 0; i < N; i++) {
+        printf("%d ", buffer[i]);
+    }
+    printf("]\n");
+    printf("Maior elemento encontrado: %d\n", maior_final);
   }
-  
+
   MPI_Finalize();
 }
+
+// mpicc -o max_int max_int.c
+// mpirun -np 4 ./max_int
